67c6f57ef0669a919893f5100b82d348ffd187cb
Shujaat <shujaat@hotmail.com>
2019-12-04 17:30:52 +0500

    - vue-router with 3 (empty) views working

---
+++ vue/src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex xs12>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+
+  data: () => ({
+  }),
+};
+</script>
--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+  configureWebpack: {
+    devtool: 'source-map'
+  },
+  devServer: {
+    proxy: 'http://hfq:8080/api/v1',
+  },
+  outputDir: '../public',
+
+  // modify the location of the generated HTML file.
+  // make sure to do this only in production.
+  indexPath: process.env.NODE_ENV === 'production'
+  ? '../resources/views/welcome.blade.php'
+  : 'index.html'
 }
