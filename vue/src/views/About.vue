19baceb13289c09857f731328914f9b81c41072e
Shujaat <shujaat@hotmail.com>
2020-01-05 12:04:17 +0500

    - added ad hoc content to About page

--- vue/src/views/About.vue
+++ vue/src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4" justify-center>
+        <div class="flex d-flex justify-center display-4 font-weight-regular">HFQ Server</div>
+        <br />
+        <div class="flex d-flex justify-center display-2 font-weight-thin">Version 1.0.5</div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
