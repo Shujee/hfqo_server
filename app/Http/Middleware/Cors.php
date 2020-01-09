6d9929454bb2eb37891685ea89a844ca1fae65a9
Shujaat <shujaat@hotmail.com>
2020-01-09 12:16:36 +0500

    - CORS fixed for local testing - Removed unnecessary front-end files from Laravel resources folder

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,8 @@ class Cors
         {
             return $next($request)
                     ->header('Access-Control-Allow-Origin', '*')
-                    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+                    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                    ->header('Access-Control-Allow-Headers', '*');
         }
         else {
             return $next($request);
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 Route::prefix('v1')->middleware('cors')->group(function()
 {
-    Route::post('/login', 'AuthController@login')->middleware('throttle:15,5')->name('login');
+    if(env('APP_DEBUG')) {
+        Route::options('/{any?}', function() {})->where('any', '.*');
+    }
+
+    Route::post('/login', 'AuthController@login')->name('login');
 
     Route::group(['middleware' => 'auth:api'], function()
     {
