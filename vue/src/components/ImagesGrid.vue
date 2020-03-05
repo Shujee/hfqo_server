5ab32c2249dce46f363c7f5389ac9475177d8bf6
Shujaat <shujaat@hotmail.com>
2020-03-05 14:19:32 +0500

    - Fixed images gallery - vue build

--- vue/src/components/ImagesGrid.vue
+++ vue/src/components/ImagesGrid.vue
@@ -1,25 +1,31 @@
 <template>
   <v-card flat class="ma-4">
-      <v-card-text class="overflow-hidden py-0">
-        <v-layout class="justify-start align-center" row v-vuse-scrollbar:x>
-          <v-flex 
-            v-for="media in images"
-            :key="media.filename"
-            px-5
-            pb-2
-            class="flex-0"
-          >
-            <v-row class="justify-center">
-              <img :src="media.filename" height="70px" width="auto">
-            </v-row>
-            <v-row class="justify-center">
-                 {{media.timestamp}}
-            </v-row>
-          </v-flex>
-        </v-layout>
-      </v-card-text>
-    </v-card>
-</template>
+    <v-col cols="12">
+      <v-card>
+        <v-container fluid>
+          <v-row>
+            <v-col v-for="media in images" :key="media.filename" class="d-flex child-flex" cols="2">
+              <v-card flat tile class="d-flex">
+                <v-img
+                  :src="media.filename"
+                  :lazy-src="media.filename"
+                  aspect-ratio="1"
+                  class="grey lighten-2"
+                >
+                  <template v-slot:placeholder>
+                    <v-row class="fill-height ma-0" align="center" justify="center">
+                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                    </v-row>
+                  </template>
+                </v-img>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-col>
+  </v-card>
+</template>             
 <script>
 export default {
   props: {
