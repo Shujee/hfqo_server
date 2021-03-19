e62733e42f54dc937fa85fb835b81c4da956d4dc
Shujaat <shujaat@hotmail.com>
2021-03-19 15:19:46 +0500

    - OAuth's "access defined" is not reported in log file now. - Added null check in AccessResource

--- app/Http/Resources/Access.php
+++ app/Http/Resources/Access.php
@@ -14,9 +14,11 @@ class Access extends JsonResource
      */
     public function toArray($request)
     {
+        $U = $this->User;
+
         return [
             'id' => $this->id,
-            'user_name' => $this->User->name,
+            'user_name' => $U == null ? '' : $U->name,
             'start' => $this->start->toIso8601String(),
             'end' => $this->end->toIso8601String(),
             'updated_at' => $this->updated_at->toIso8601String(),
--- vue/vue.config.js
+++ vue/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
     chainWebpack: config => {
         config.plugin("html").tap(args => {
-            args[0].minify.removeAttributeQuotes = false;
+            //args[0].minify.removeAttributeQuotes = false;
             return args;
         });
     },
